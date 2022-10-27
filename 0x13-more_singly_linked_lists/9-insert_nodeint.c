760279d993e0c9eebde6880a4ef2f0c175c43fb9
DWarwere <warweredickson@gmail.com>
2022-10-27 20:56:44 +0000

    Func that returns the number of elelments in a listint_t list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,47 +1,60 @@
 #include "lists.h"
+
 /**
- * insert_nodeint_at_index - This function inserts a
- * new node at given position
- * @head: The head node
- * @idx: The index of the list where new node is to be added
- * @n: The integer data of the new node
- * Return: The address of the new inserted node or NULL if failed
+ * listint_len - func that returns the no of elements in a listint_t list
+ * @h: pointer to a listint_t struct
+ * Return: no of elements
  */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+
+	}
+	return (counter);
+
+}
+
+/**
+ * insert_nodeint_at_index - func to insert a new node at a given position
+ * @head: pointer pointing to the beginning of a listint_t list
+ * @idx: index where new node should be added
+ * @n: value of n member for the new node
+ * Return: address of the new node or NULL on failure
+ */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node, *temp = *head;
-	size_t i = 0;
+	size_t len;
+	unsigned int i;
+	listint_t *new_node;
+	listint_t *prev_node = *head;
 
-	node = malloc(sizeof(listint_t));
-	if (!node)
+	len = listint_len(*head);
+
+	if (idx > len)
 		return (NULL);
 
-	node->n = n;
-	node->next = NULL;
-
-	if (!*head && !idx)
-	{
-		*head = node;
-		return (node);
-	}
-
-	else if (!*head && idx)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
+	new_node->n = n;
 
-	else if (!idx)
+	if (idx == 0)
 	{
-		node->next = temp;
-		*head = node;
-		return (node);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
+	for (i = 0; i < idx - 1; i++)
+		prev_node = prev_node->next;
+	new_node->next = prev_node->next;
+	prev_node->next = new_node;
+	return (new_node);
 
-	while (i < (idx - 1))
-	{
-		temp = temp->next;
-		i++;
-	}
-	node->next = temp->next;
-	temp->next = node;
-
-	return (node);
 }
